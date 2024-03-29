82df7b00d3d9e07af35b7a7ff6531a78efbd6653
APIAutomationRestAssured5X <GurpreetBatth87@gmail.com>
2024-03-29 23:43:26 +0530

    API AUTOMATION updateE

--- src/main/java/org/Automation5x/endpoints/APIConstants.java
+++ src/main/java/org/Automation5x/endpoints/APIConstants.java
@@ -1,7 +1,28 @@
 package org.Automation5x.endpoints;
 
+import org.Automation5x.utils.PropertyReader;
+
 public class APIConstants {
-    public static String BASE_URL = "https://restful-booker.herokuapp.com/";
+   public static String BASE_URL = "https://restful-booker.herokuapp.com/";
+   // public static String BASE_URL;
+
+//    static {
+//        try {
+//            BASE_URL = PropertyReader.readkey("url");
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
     public static String CREATE_PDATE_BOOKING = "booking";
 
 }
+
+
+
+//allure serve allure-results
+//
+//npm i allure-commandline
+//npm i -g allure-commandline
+//
+//import io.qameta.allure.Description;
--- src/main/java/org/Automation5x/utils/PropertyReader.java
+++ src/main/java/org/Automation5x/utils/PropertyReader.java
@@ -2,13 +2,10 @@ package org.Automation5x.utils;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Properties;
 
 public class PropertyReader {
-    public PropertyReader() {
-    }
+
 
     public static String readkey(String key) throws Exception {
         FileInputStream fileInputStream = new FileInputStream(new File(System.getProperty("user.dir")+"src/test/resources/TestDataProperties.properties"));
